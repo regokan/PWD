67af2de8074387286b4cbf9897867239fe1f280c
Kanish Gosain <regokan@gmail.com>
2020-06-21 14:39:05 +0530

    Debugging the callback pattern

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const client = redis.createClient({
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
-  const visits = parseInt(client.get("visits"));
-  client.set("visits", visits + 1);
-  res.send(`Number of visits ${visits + 1}`);
+  const visits = client.get("visits", (err, visits) => {
+    client.set("visits", parseInt(visits) + 1);
+    res.send(`Number of visits ${parseInt(visits) + 1}`);
+  });
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
